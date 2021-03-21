7bb49074d08872fb1d61e0cca560d48cfebbc045
seshu418 <seshu418@gmail.com>
2021-03-21 22:13:29 +0530

    After first download

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb+srv://apiuser:ltUHrhaNnXEvVjgQ@cluster0.ydujp.mongodb.net/NodeJSRestfulAPI?retryWrites=true&w=majority',{ useNewUrlParser: true ,useUnifiedTopology: true});
